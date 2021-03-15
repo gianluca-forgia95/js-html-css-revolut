a6d7d708af0a5e92407e642bf30ea85ae805eec4
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-03-15 14:47:29 +0100

    aggiungo evento al click sulla nav

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
+//Variabili utili
+var navItems = $('.menu-drop  li');
+console.log(navItems);
 
+//Evento click sulla nav
+navItems.click(
+  function () {
+   $('.dropdown-menu').fadeIn();
+  }
+);
